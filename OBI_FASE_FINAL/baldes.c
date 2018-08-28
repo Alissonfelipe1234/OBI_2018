9f51500e260fdcab3a04ed07b123f2345116ec3e
Alisson Felipe <alissonf@cpqd.com.br>
2018-08-28 12:58:13 -0300

    Alteração no programa baldes.c para validação das operações

--- OBI_FASE_FINAL/baldes.c
+++ OBI_FASE_FINAL/baldes.c
@@ -15,11 +15,39 @@ int main()
     }
     for (i = 0; i < operacoes; i++)
     {
-        scanf("%d %d %d", &oper, x, y);
-        y--;
+        scanf("%d %d %d", &oper, x, y);        
         if(oper == 1)
         {
-
+            y--;
+            baldes[y][quantasBolas[y]] = x;
+            quantasBolas[y]++;
+        }
+        else
+        {
+            if(x < y)
+            {
+                x--;
+                register menor = baldes[x][0], maior = baldes[x][0], menorIndex[2] = {x,0}, maiorIndex[2] = {x,0};
+                for (register int g = x; g<y; i++)
+                {
+                    for(register int f = 0; f < quantasBolas[g]; f++)
+                    {
+                        if (menor > baldes[g][f])
+                        {
+                            menor = baldes[g][f];
+                            menorIndex[0] = g;
+                            menorIndex[1] = f;
+                        }
+                        if (maior < baldes[g][f])
+                        {
+                            menor = baldes[g][f];
+                            menorIndex[0] = g;
+                            menorIndex[1] = f;
+                        }
+                    }
+                }
+                printf("%i\n", maior - menor);                
+            }
         }
     }
 }
